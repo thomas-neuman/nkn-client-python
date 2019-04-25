250950bc1748a2c6be71a8742babe96c47d868c5
thomas.neuman <thomas.neuman@nutanix.com>
2019-04-24 22:20:12 -0700

    Wrapped up the tests

--- test/websocket/test_nkn_api.py
+++ test/websocket/test_nkn_api.py
@@ -167,7 +167,19 @@ class TestNknWebsocketApiClient(asynctest.TestCase):
       _ = await self._client.get_block(hash="deadbeef")
 
   async def test_get_connection_count_success(self):
-    pass
+    expected = 3
+    mock_call = CoroutineMock(return_value={
+      "Action": "getconnectioncount",
+      "Error": 0,
+      "Desc": "SUCCESS",
+      "Result": expected,
+      "Version": "1.0.0"
+    })
+    self._client.call_rpc = mock_call
+
+    actual = await self._client.get_connection_count()
+
+    self.assertEqual(actual, expected)
 
   async def test_get_connection_count_error(self):
     mock_call = CoroutineMock(return_value={
@@ -183,7 +195,24 @@ class TestNknWebsocketApiClient(asynctest.TestCase):
       _ = await self._client.get_connection_count()
 
   async def test_get_transaction_success(self):
-    pass
+    expected = {
+      "hash": "327bb43c2e40ccb2f83011d35602829872ab190171b79047397d000eddda18a9",
+      "inputs": "..."
+    }
+    mock_call = CoroutineMock(return_value={
+      "Action": "gettransaction",
+      "Error": 0,
+      "Desc": "SUCCESS",
+      "Result": expected,
+      "Version": "1.0.0"
+    })
+    self._client.call_rpc = mock_call
+
+    actual = await self._client.get_transaction(
+      "327bb43c2e40ccb2f83011d35602829872ab190171b79047397d000eddda18a9"
+    )
+
+    self.assertEqual(actual, expected)
 
   async def test_get_transaction_error(self):
     mock_call = CoroutineMock(return_value={
@@ -199,7 +228,19 @@ class TestNknWebsocketApiClient(asynctest.TestCase):
       _ = await self._client.get_transaction("deadbeef")
 
   async def test_heartbeat_success(self):
-    pass
+    expected = "5a232d0c-79ea-11e8-a7f5-6a00030e74b0"
+    mock_call = CoroutineMock(return_value={
+      "Action": "heartbeat",
+      "Error": 0,
+      "Desc": "SUCCESS",
+      "Result": expected,
+      "Version": "1.0.0"
+    })
+    self._client.call_rpc = mock_call
+
+    actual = await self._client.heartbeat()
+
+    self.assertEqual(actual, expected)
 
   async def test_heartbeat_error(self):
     mock_call = CoroutineMock(return_value={
@@ -215,7 +256,19 @@ class TestNknWebsocketApiClient(asynctest.TestCase):
       _ = await self._client.heartbeat()
 
   async def test_get_session_count_success(self):
-    pass
+    expected = 1
+    mock_call = CoroutineMock(return_value={
+      "Action": "getsessioncount",
+      "Error": 0,
+      "Desc": "SUCCESS",
+      "Result": expected,
+      "Version": "1.0.0"
+    })
+    self._client.call_rpc = mock_call
+
+    actual = await self._client.get_session_count("127.0.0.1:30002")
+
+    self.assertEqual(actual, expected)
 
   async def test_get_session_count_error(self):
     mock_call = CoroutineMock(return_value={
@@ -231,7 +284,19 @@ class TestNknWebsocketApiClient(asynctest.TestCase):
       _ = await self._client.get_session_count("127.0.0.1:30002")
 
   async def test_set_client_success(self):
-    pass
+    expected = None
+    mock_call = CoroutineMock(return_value={
+      "Action": "setclient",
+      "Error": 0,
+      "Desc": "SUCCESS",
+      "Result": expected,
+      "Version": "1.0.0"
+    })
+    self._client.call_rpc = mock_call
+
+    actual = await self._client.set_client("anything.pubkey")
+
+    self.assertEqual(actual, expected)
 
   async def test_set_client_error(self):
     mock_call = CoroutineMock(return_value={
@@ -247,7 +312,23 @@ class TestNknWebsocketApiClient(asynctest.TestCase):
       _ = await self._client.set_client("anything.pubkey")
 
   async def test_send_packet_success(self):
-    pass
+    expected = None
+    mock_call = CoroutineMock(return_value={
+      "Action": "sendPacket",
+      "Error": 0,
+      "Desc": "SUCCESS",
+      "Result": expected,
+      "Version": "1.0.0"
+    })
+    self._client.call_rpc = mock_call
+
+    actual = await self._client.send_packet(
+        "nkn.address",
+        "message",
+        "signature"
+    )
+
+    self.assertEqual(actual, expected)
 
   async def test_send_packet_error(self):
     mock_call = CoroutineMock(return_value={
@@ -263,7 +344,39 @@ class TestNknWebsocketApiClient(asynctest.TestCase):
       _ = await self._client.send_packet("nkn.address", "message", "sig")
 
   async def test_get_incoming_packet(self):
-    pass
+    Action = "receivePacket"
+    Src = "source"
+    Payload = "payload"
+    Digest = "digest"
 
-  async def test_sig_block_chain_hash(self):
-    pass
+    await self._client.receive_packet(
+        Action=Action,
+        Src=Src,
+        Payload=Payload,
+        Digest=Digest
+    )
+
+    asrc, apayload, adigest = await self._client.get_incoming_packet()
+
+    self.assertEqual(asrc, Src)
+    self.assertEqual(apayload, Payload)
+    self.assertEqual(adigest, Digest)
+
+  async def test_sig_chain_block_hash(self):
+    Action = "updateSigChainBlockHash"
+    Error = 0
+    Desc = ""
+    Result = "result"
+    Version = "version"
+
+    await self._client.update_sig_chain_block_hash(
+        Action=Action,
+        Error=Error,
+        Desc=Desc,
+        Result=Result,
+        Version=Version
+    )
+
+    actual = self._client.sig_chain_block_hash
+
+    self.assertEqual(actual, Result)
