42715c51011c22a6a0e90b412f6b57cb526f29fe
Thomas Neuman <neuman.tn@gmail.com>
2019-04-29 21:05:21 -0700

    Add end-to-end test

---
+++ test/test_end_to_end.py
@@ -0,0 +1,31 @@
+import asyncio
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
+
+import nkn_client
+
+async def main():
+  from_client = nkn_client.NknClient("one")
+  to_client = nkn_client.NknClient("two")
+
+  print("FROM:", from_client.address)
+  print("TO:", to_client.address)
+
+  await from_client.connect()
+  print(from_client._ws)
+
+  await to_client.connect()
+  print(to_client._ws)
+
+  destination = to_client.address
+  payload = "It's alive!"
+
+  await from_client.send(destination, payload)
+  print("Sent!")
+
+  packet = await to_client.recv()
+  assert packet.payload == payload
+
+if __name__ == "__main__":
+  asyncio.run(main())
